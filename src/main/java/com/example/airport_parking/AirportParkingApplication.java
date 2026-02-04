1570ad77ae74eb2d4f5e9402d6c4933f9d6a14dd
concho <dlwnsdn980828@gmail.com>
2026-02-04 13:40:22 +0900

    최초커밋

---
+++ src/main/java/com/example/airport_parking/AirportParkingApplication.java
@@ -0,0 +1,13 @@
+package com.example.airport_parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportParkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirportParkingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/airport_parking/AirportParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airport_parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportParkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
